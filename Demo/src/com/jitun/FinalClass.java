fb7554e6e70dc3cec11836cb3b4e41df5825d3ea
jitunpanda <jitunpanda@yahoo.in>
2017-03-25 16:16:08 +0530

    first change

--- Demo/src/com/jitun/FinalClass.java
+++ Demo/src/com/jitun/FinalClass.java
@@ -3,6 +3,11 @@ package com.jitun;
 public final class FinalClass {
 	final String finalMethod() { return "something"; }
 	public void test() {
-		
+		System.out.println("test 1");
+	}
+	public static void main(String[] args) {
+		FinalClass finalClass=new FinalClass();
+		System.out.println(finalClass.toString());
+		finalClass.test();
 	}
 }
